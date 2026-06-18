5bc0f76fa699fe5633f036766cbe0ced9449f840
Lorenzo Faraoni <lorefara97@gmail.com>
2026-06-18 10:52:36 +0200

    🏋️ Deploy 2026-06-18 10:52

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * (le chiamate API richiedono sempre internet)
  */
 
-const CACHE_NAME = "fitness-hub-v3-20260618104551";
+const CACHE_NAME = "fitness-hub-v3-20260618105236";
 const BASE = "/fitness-hub";
 
 const STATIC_ASSETS = [
